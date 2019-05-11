e0bdbca85608300cdcedc79598d50fbc86ff8a3b
Kira Sl <42243887+KiraSl@users.noreply.github.com>
2019-05-11 18:06:56 +0100

    create an array with pokemons

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var repository = [
+  { name: 'Wartorle', height: 1, types: ['water'] },
+  { name: 'Nidorino', height: 0.9, types: ['poison'] },
+  { name: 'Oddish', height: 0.5, types: ['grass', 'poison'] },
+  { name: 'Infernape', height: 1.2, types: ['fire', 'fighting'] },
+  { name: 'Hypno', height: 1.6, types: ['psychic'] }
+];
+
