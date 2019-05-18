853691d1d7c4bf378d3d302b5a452bcdd7b5671a
Kira Sl <42243887+KiraSl@users.noreply.github.com>
2019-05-18 18:25:29 +0100

    Wrap repository array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-var repository = [
-  { name: 'Wartorle', height: 1, types: ['water'] },
-  { name: 'Nidorino', height: 0.9, types: ['poison'] },
-  { name: 'Oddish', height: 0.5, types: ['grass', 'poison'] },
-  { name: 'Infernape', height: 1.2, types: ['fire', 'fighting'] },
-  { name: 'Hypno', height: 1.6, types: ['psychic'] }
-];
+var pokemonRepository = (function () {
+  var repository = [
+    { name: 'Wartorle', height: 1, types: ['water'] },
+    { name: 'Nidorino', height: 0.9, types: ['poison'] },
+    { name: 'Oddish', height: 0.5, types: ['grass', 'poison'] },
+    { name: 'Infernape', height: 1.2, types: ['fire', 'fighting'] },
+    { name: 'Hypno', height: 1.6, types: ['psychic'] }
+  ];
+})();
 
 repository.forEach(function(element) {
   document.write(`<h3>${element.name}</h3>` + `<p>height: ${element.height}, types: ${element.types}</p>`);
